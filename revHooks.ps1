6f61f3d648fcb2b8bc226cff80c2f549cd51e5c3
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2022-05-13 18:09:57 -0600

    Initial version of Powershell Patcher script

---
+++ revHooks.ps1
@@ -0,0 +1,114 @@
+# Configure for your installation
+$installPath = "$($env:LOCALAPPDATA)\Vivaldi\Application"
+
+#  ==============================================================================
+function Restore-Bundle {
+  param ($path)
+  Write-Host "* Restoring bundle.js from bundle.js.bak"
+  Copy-Item "$path\bundle.js.bak" -Destination "$path\bundle.js"
+}
+function Backup-Bundle {
+  param ($path)
+  Write-Host "* Backing up current bundle.js as bundle.js.bak"
+  Copy-Item "$path\bundle.js" -Destination "$path\bundle.js.bak"
+}
+
+function Wait-And-Exit {
+  # If running in the console, wait for input before closing.
+  if ($Host.Name -eq "ConsoleHost") {
+    Write-Host "Press any key to EXIT..."
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+    Exit
+  }
+}
+
+
+# find the path to the most recent version of Vivaldi
+Get-ChildItem $installPath -Filter browser.html -Recurse | % { 
+  $latestVersionFolder = $_.FullName -replace "\\browser.html", "" 
+}
+
+# make sure the path was found
+if ($latestVersionFolder) {
+  Write-Host "* Found latest version at: $latestVersionFolder"
+}
+else {
+  Write-Warning "[Error] No instances of Vivaldi found at the provided install path: $installPath"
+  Wait-And-Exit
+}
+
+
+# save a copy of the unedited file or follow user choice
+if ((Test-Path "$latestVersionFolder\bundle.js.bak")) {
+  Write-Warning "[Action Required] Backup of bundle.js found."
+  Do {
+    $answer = Read-host "Choose an option and enter the corresponding number:`
+  - (1) [Recommended] Restore bundle.js from the backup and CONTINUE`
+  - (2) Restore bundle.js from the backup and EXIT`
+  - (3) Delete the backup and create a new one before CONTINUING`r`
+Choice"
+  }
+  while ((1..3) -notcontains $answer )
+}
+
+switch ($answer) {
+  1 {
+    Restore-Bundle $latestVersionFolder
+    Backup-Bundle $latestVersionFolder
+  }
+  2 {
+    Restore-Bundle $latestVersionFolder
+    Remove-Item -Path "$latestVersionFolder\bundle.js.bak"
+    Wait-And-Exit
+  }
+  Default {
+    Backup-Bundle $latestVersionFolder
+  }
+}
+
+# load contents of bundle.js
+$bundleJS = "$latestVersionFolder\bundle.js"
+
+# find the JSON hook files and implement the changes
+Write-Host "* Starting to patch in hooks"
+$scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+Get-ChildItem $scriptPath -Filter *.json -Recurse | % { 
+  $hookJson = Get-Content $_.FullName -Raw | ConvertFrom-Json
+  $tempBundle = Get-Content $bundleJS -Raw
+
+  Write-Host "---"
+  Write-Host "* Patching in hook $_"
+  
+  $changeNumber = 0
+  $saveChanges = $true
+  ForEach ($change in $hookJson.replacements) {
+    if (($change.find.Length -lt 2) -or ($change.replace.Length -lt 2)) {
+      Write-Warning "[Invalid] Change number $changeNumber is invalid"
+      $saveChanges = $false
+      break
+    }
+    if ($tempBundle.Contains($change.find)) {
+      Write-Host "** Made change number $changeNumber"
+      $tempBundle = $tempBundle.Replace($change.find, $change.replace)
+      $changeNumber = $changeNumber + 1
+    }
+    else {
+      Write-Warning "[Failure] Failed to find change number $changeNumber"
+      $saveChanges = $false
+      break
+    }
+  }
+
+  # if changes were succefully made apply them to bundle.js
+  if ($saveChanges) {
+    Write-Host "** Saving changes to bundle.js"
+    $tempBundle | Out-File $bundleJS
+  }
+  else {
+    Write-Host "** Hook not applied to bundle.js"
+  }
+}
+
+Write-Host "==="
+
+Wait-And-Exit
